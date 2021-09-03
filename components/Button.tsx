1c8873ecb041ac8ae2a97d96b83ae15dcdd14e06
masafumi <masa455.wv@gmail.com>
2021-09-03 19:12:32 +0900

    add:ボタンのスタイルとpropsを追加

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,26 @@
+import styles from './Button.module.css'
+
 // propsの型指定
 type TodoType = {
   title: string
+  message: string
+  color: string
+  backColor: string
 }
 
 export const Button = (props: TodoType) => {
-  const { title } = props
+  const { title, message, color, backColor } = props
 
-  const onclickLink = () => alert()
-
+  const onclickLink = () => alert(message)
   return (
     <div>
-      <button onClick={onclickLink}>{title}</button>
+      <button
+        onClick={onclickLink}
+        className={styles.button}
+        style={{ background: backColor, color: color }}
+      >
+        {title}
+      </button>
     </div>
   )
 }
